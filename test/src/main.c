74158ef31f223d9887cca95bdf80ec8fa481faf8
= <zimlit2010@gmail.com>
2023-08-13 13:04:46 -0400

    unit testing

--- test/src/main.c
+++ test/src/main.c
@@ -17,23 +17,42 @@
  * along with walnut.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <check.h>
 #include <walnut/walnut.h>
-#include <walnut/debug.h>
-#include <stddef.h>
-#include <stdio.h>
 
-int main() {
+START_TEST(test_load) {
     Walnut walnut;
     uint64_t code[] = {
-        0x1000000000A0000,
-        0x1010000000A0000,
-        0x1300010000000000,
+        0x100000000040000,
     };
-    walnutDisassemble(code, 3);
-    walnutInit(&walnut, code, 3);
+    walnutInit(&walnut, code, 1);
     walnutRun(&walnut);
-    walnutDumpRegisterFile(&walnut);
-    printf("%llx\n", walnut.mem.data[0x10]);
+    ck_assert_uint_eq(walnut.registers[0], 4);
     walnutFree(&walnut);
-    return 0;
+}
+END_TEST
+
+Suite *walnut_suite() {
+    Suite *s;
+    TCase *tc_core;
+    s = suite_create("Walnut");
+    tc_core = tcase_create("Core");
+    tcase_add_test(tc_core, test_load);
+    suite_add_tcase(s, tc_core);
+    
+    return s;
+}
+
+int main() {
+    int numberFailed;
+    Suite *s;
+    SRunner *sr;
+    
+    s = walnut_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_NORMAL);
+    numberFailed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (numberFailed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
