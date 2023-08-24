94b4140520b40dd61c0aafcc6fff8d062760ad47
= <zimlit2010@gmail.com>
2023-08-24 15:00:29 -0400

    remove bad assembler code

--- walnut/src/assembler/assembler.c
+++ walnut/src/assembler/assembler.c
@@ -49,102 +49,11 @@ walnutAssemblerOutputFree(WalnutAssemblerOutput *output)
   walnutAssemblerOutputInit(output);
 }
 
-#define ERROR(msg, line, col)                                                 \
-  fprintf(stderr, "ERROR: " msg " at %d:%d\n", line, col)
-
-// TODO: error handling
 WalnutAssemblerOutput
 walnutAssemble(const char *source)
 {
   WalnutAssemblerOutput output;
   walnutAssemblerOutputInit(&output);
-
-  int i    = 0;
-  int col  = 0;
-  int line = 0;
-  while (source[i] != '\0')
-    {
-      switch (source[i])
-        {
-        case 'l':
-          {
-            switch (source[i + 1])
-              {
-              case 'd':
-                {
-                  switch (source[i + 2])
-                    {
-                    case 'i':
-                      {
-                        uint64_t instr = (uint64_t)WalnutOpLdi << 56;
-                        i += 3;
-                        col += 3;
-                        while (isspace(source[i]))
-                          {
-                            i++;
-                            col++;
-                          }
-                        i++;
-                        col++;
-                        char *end;
-                        uint8_t reg = strtoul(source + i, &end, 10);
-                        if (reg > 15)
-                          {
-                            ERROR("register number may not be higher than 15",
-                                  line, col);
-                          }
-                        instr |= (uint64_t)reg << 48;
-                        col += (end - source) - i;
-                        i = end - source;
-
-                        if (source[i] != ',')
-                          {
-                            ERROR("expected comma", line, col);
-                          }
-                        i++;
-                        col++;
-
-                        while (isdigit(source[i]))
-                          {
-                            if (!isspace(source[i]))
-                              {
-                                ERROR("expected number", line, col);
-                              }
-                            i++;
-                            col++;
-                          }
-                        uint32_t val = strtoul(source + i, &end, 10);
-                        instr |= (uint64_t)val << 16;
-                        col += (end - source) - i;
-                        i = end - source;
-                        walnutAssemblerOutputPush(&output, instr);
-                        break;
-                      }
-                    }
-                  break;
-                }
-              }
-            break;
-          }
-        default:
-          if (isspace(source[i]))
-            {
-              i++;
-              if (source[i] == '\n')
-                {
-                  line++;
-                  col = 0;
-                }
-            }
-          else
-            {
-              ERROR("unknown character", line, col);
-              output.hadError = true;
-              i++;
-            }
-          break;
-        }
-    }
 
   return output;
 }
